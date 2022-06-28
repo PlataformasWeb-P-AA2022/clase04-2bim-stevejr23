7806d95fb042df52f804a769b0fb6946f1507161
stevejr23 <stevej4r412@hotmail.com>
2022-06-28 18:10:14 -0500

    clase4

--- ejemplo5/proyectoUno/administrativo/models.py
+++ ejemplo5/proyectoUno/administrativo/models.py
@@ -14,6 +14,17 @@ class Estudiante(models.Model):
                 self.cedula,
                 self.correo)
 
+    def get_provincia (self):
+        
+        dato =self.cedula[0:2]
+        valor = "Sin Provincia"
+        if dato == "11":
+            valor = "Loja"
+        else:
+            if dato == "17":
+                valor = "Pichincha"
+        return valor
+
 class NumeroTelefonico(models.Model):
     telefono = models.CharField(max_length=100)
     tipo = models.CharField(max_length=100)
